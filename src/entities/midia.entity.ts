c4dc28fd13175f7271af056f07fce837a893f973
Gabriel Dias <dias.1618@gmail.com>
2020-08-18 00:49:41 +0000

    fix(midia): Correção de propriedades da classe

--- src/entities/midia.entity.ts
+++ src/entities/midia.entity.ts
@@ -4,10 +4,14 @@ import { GrupoMidia } from "./grupo-midia.entity";
 @Entity()
 export class Midia extends BaseEntity{
 
-    constructor(data: {id?:number, titulo?:string, icone?:any}){
+    constructor(data: {id?:number, titulo?:string, icone?:any, situacaoMidia?:number, situacaoAcompanhamento?:number, ultimoVisto?:number}){
         super();
         this.id = data && data.id || 0;
         this.titulo = data && data.titulo || "";
+        this.icone = data && data.icone || null;
+        this.situacaoMidia = data && data.situacaoMidia || 0;
+        this.situacaoAcompanhamento = data && data.situacaoAcompanhamento || 0;
+        this.ultimoVisto = data && data.ultimoVisto || 0;
     }
 
     @PrimaryGeneratedColumn()
