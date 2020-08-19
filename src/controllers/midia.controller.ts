1a6f7a94843f945748efa65b40c50c48acd191b8
Gabriel Dias <dias.1618@gmail.com>
2020-08-18 23:26:45 -0300

    feature(midia): Busca de midia a partir do id do grupo

--- src/controllers/midia.controller.ts
+++ src/controllers/midia.controller.ts
@@ -1,4 +1,4 @@
-import { Post, HttpCode, Body, Controller } from "@nestjs/common";
+import { Post, HttpCode, Body, Controller, Get, Query } from "@nestjs/common";
 import { Midia } from "src/entities/midia.entity";
 import { MidiaService } from "src/services/midia.service";
 import { MidiaNaoCadastradaException } from "src/exceptions/midia-nao-cadastrada.exception";
@@ -12,13 +12,18 @@ export class MidiaController{
 
   @Post()
   @HttpCode(200)
-  async login(@Body() midia: Midia) {
+  async save(@Body() midia: Midia) {
     midia = await this.midiaService.create(midia);
 
     if(!midia)
         throw new MidiaNaoCadastradaException();
 
     return midia;
+  }
+
+  @Get()
+  async get(@Query('idGrupoMidia') idGrupoMidia:number){
+    return await this.midiaService.getAllByGrupoMidia(idGrupoMidia);
   }
 
 }
--- src/services/midia.service.ts
+++ src/services/midia.service.ts
@@ -1,10 +1,16 @@
 import { Midia } from "src/entities/midia.entity";
+import { getRepository } from "typeorm";
 
 export class MidiaService{
     async create(midia:Midia):Promise<Midia>{
-        console.log(midia);
         midia = new Midia(midia);
-        console.log(midia);
         return await midia.save();
+    }
+
+    async getAllByGrupoMidia(idGrupoMidia:number):Promise<Midia[]>{
+        return await getRepository(Midia).createQueryBuilder('midia')
+        .leftJoinAndSelect('midia.grupoMidia', 'grupoMidia')
+        .where('grupoMidia.id = :id', {id: idGrupoMidia})
+        .getMany();
     }
 }
